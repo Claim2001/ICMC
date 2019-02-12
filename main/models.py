c4ec4a252b4469f3bbb856332ae476caa90b5567
Dils Matchanov <dils.matchanov@gmail.com>
2019-02-12 14:52:31 +0500

    Added TODOS

--- main/models.py
+++ main/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+# TODO: read whole documentation and build the skeleton for the project
+# TODO: build models
+# TODO: custom authentication
+# 
+# TODO: create normal README so people can see what I am doing
 
 class CustomUser(AbstractUser):
     age = models.IntegerField(blank=True, null=True)
