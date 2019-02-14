eab8b764083215eb40eb734f3ff14d126fcb1dc0
dilsmatchanov <dils.matchanov@gmail.com>
2019-02-14 02:46:56 -0500

    Cleaned code up

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, QueryDict
 from django.views.generic import View
 from django.contrib.auth import authenticate, login
 
@@ -37,42 +37,37 @@ class Login(View):
         return redirect("main:login")
 
 
+def remove_useless_elements_from_post(post):
+    """
+    I wrote this function because I wanted to 
+    use nice ```user = Owner(**post)``` syntax instead
+    of extracting every field one by one but 
+    ```request.POST``` has a bunch of elements
+    that should not be passed as arguments when create
+    an instance of a model
+    """
+    post_copy = post.copy()
+
+    post_copy.pop("csrfmiddlewaretoken")
+    post_copy.pop("password")
+
+    qdict = QueryDict('', mutable=True)
+    qdict.update(post_copy)
+
+    return qdict
+
+
 class SignUp(View):
     def get(self, request):
         return render(request, "main/signup.html", {})
 
     def post(self, request):
-        # wtf is this? Didn't find a better solution :(
-        username = request.POST['username']
-        second_name = request.POST['second_name']
-        gender = request.POST['gender']
-        organization_name = request.POST['organization_name']
-        address = request.POST['address']
-        mail_index = request.POST['mail_index']
-        date_of_passport = request.POST['date_of_passport']
-        inn = request.POST['inn']
-        country = request.POST['country']
-        city = request.POST['city']
-        email = request.POST['email']
-        phone_number = request.POST['phone_number']
+        post = remove_useless_elements_from_post(request.POST)
+        user = Owner(**post)
+
         password = request.POST['password']
-
-        user = Owner(
-            username=username,
-            second_name=second_name,
-            gender=gender,
-            name_of_organization=organization_name,
-            address=address,
-            mail_index=mail_index,
-            date_of_passport=date_of_passport,
-            inn=inn,
-            country=country,
-            city=city,
-            email=email,
-            phone_number=phone_number
-        )
-
         user.set_password(password)
+
         user.save()
 
         login(request, user)
