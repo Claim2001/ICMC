686af53e1498e993e8b5bf5ecfc773e2f3fd1957
dilsmatchanov <dils.matchanov@gmail.com>
2019-02-25 16:33:32 +0500

    Worked on form

--- main/admin.py
+++ main/admin.py
@@ -4,23 +4,20 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import ReadOnlyPasswordHashField, UserChangeForm
 
 from .models import Owner, Boat
+from .forms import UserForm
 
 # TODO: can't change password in admin panel
 
-# class OwnerAdminForm(UserChangeForm):
-#     password = ReadOnlyPasswordHashField(label=("Password"),
-#         help_text=("Raw passwords are not stored, so there is no way to see "
-#                     "this user's password, but you can change the password "
-#                     "using <a href=\"../password/\">this form</a>."))
+class OwnerChangeForm(UserForm):
+    password = ReadOnlyPasswordHashField(label= ("Password"),
+        help_text= ("Raw passwords are not stored, so there is no way to see "
+                    "this user's password, but you can change the password "
+                    "using <a href=\"password/\">this form</a>."))
 
-#     class Meta(UserChangeForm.Meta):
-#         model = Owner
-#         fields = "__all__"
-
-
-# class OwnerAdmin(admin.ModelAdmin):
-#     form = OwnerAdminForm
+class OwnerAdmin(admin.ModelAdmin):
+    form = UserForm
+    change_form = OwnerChangeForm
 
 
-admin.site.register(Owner)
+admin.site.register(Owner, OwnerAdmin)
 admin.site.register(Boat)
